f6121d751b94066abf7853a260a091394196d54f
guetter <goettems32@gmail.com>
2025-10-06 23:29:33 -0300

    wip(): trying to get H2 to work

--- Filmes/src/main/java/br/ifsul/Filme.java
+++ Filmes/src/main/java/br/ifsul/Filme.java
@@ -1,4 +1,5 @@
 package br.ifsul;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -6,9 +7,16 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Entity
+@Table(name = "FILME")
 public class Filme {
+	@Id
+    @Column(name = "ID")
 	private Long id;
+    @Column(name = "TITULO")
 	private String titulo;
+    @Column(name = "DIRETOR")
 	private String diretor;
+    @Column(name = "ANO_LANCAMENTO")
 	private String anoLancamento;
 }
